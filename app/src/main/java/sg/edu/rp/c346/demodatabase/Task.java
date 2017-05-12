011cab5976606d7c3f46dc8ea2f78fba98702fbd
15056215 <15056215@myrp.edu.sg>
2017-05-12 16:21:20 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demodatabase/Task.java
@@ -0,0 +1,38 @@
+package sg.edu.rp.c346.demodatabase;
+
+/**
+ * Created by 15056215 on 12/5/2017.
+ */
+
+public class Task {
+    private int id;
+    private String description;
+    private String date;
+
+    public Task(int id, String description, String date) {
+        this.id = id;
+        this.description = description;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + '\n' +
+                " description='" + description + '\n' +
+                " date='" + date + '\n';
+    }
+}
+
+
